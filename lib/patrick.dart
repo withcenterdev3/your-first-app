f9869a7a30e2f21899e68141b15f6d365fbf88cf
pdbautista0 <patdi.bautista@gmail.com>
2023-08-25 09:36:17 +0800

    Added SideNavigation and FavortieisPage

--- lib/patrick.dart
+++ lib/patrick.dart
@@ -26,36 +26,161 @@ class PatrickPageState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
-class PatrickContainer extends StatelessWidget {
+class PatrickContainer extends StatefulWidget {
+  @override
+  State<PatrickContainer> createState() => _PatrickContainerState();
+}
+
+class _PatrickContainerState extends State<PatrickContainer> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = FavoritesPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+
+
+    }
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+            appBar: AppBar(
+              title: const Text('Patrick'),
+              actions: <Widget>[
+                IconButton(
+                    icon: const Icon(Icons.home),
+                    tooltip: 'Home',
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
+              ],
+            ),
+            body: Row(children: [
+              SafeArea(
+                  child: NavigationRail(
+                extended: constraints.maxWidth >= 600,
+                destinations: [
+                  NavigationRailDestination(
+                      icon: Icon(Icons.home), label: Text('Home')),
+                  NavigationRailDestination(
+                      icon: Icon(Icons.favorite), label: Text('Favorites'))
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                  
+                },
+              )),
+              Expanded(
+                  child: Container(
+                      color: Theme.of(context).colorScheme.primaryContainer,
+                      child: page))
+            ]));
+      }
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<PatrickPageState>();
     var pair = appState.current;
+    var favorites = appState.favorites;
 
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Patrick'),
-          actions: <Widget>[
-            IconButton(
-                icon: const Icon(Icons.home),
-                tooltip: 'Home',
-                onPressed: () {
-                  Navigator.pop(context);
-                }),
-          ],
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-           Text('A random AWESOME idea'),
-           BigCard(pair: pair,),
-           // Add this
-           ElevatedButton(onPressed: () {appState.getNext();},
-            child: Text('Next'))],
-           
-        ));
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('A random AWESOME idea'),
+          BigCard(
+            pair: pair,
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(favorites.contains(pair)
+                      ? Icons.favorite
+                      : Icons.favorite_border_outlined),
+                  label: Text('Like')),
+              SizedBox(
+                width: 10.0,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                  },
+                  child: Text('Next')),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    var appState = context.watch<PatrickPageState>();
+    var favorites = appState.favorites;
+
+    return ListView(children: [
+      Text('Favorites'),
+      for(var fav in favorites)
+        FavoriteChip(fav: fav), SizedBox(height: 10.0,),
+    ],);
+  }
+}
+
+class FavoriteChip extends StatelessWidget {
+  const FavoriteChip({
+    super.key,
+    required this.fav,
+  });
+
+  final WordPair fav;
+
+  @override
+  Widget build(BuildContext context) {
+    return Chip(
+      avatar: Icon(Icons.favorite),
+      label: Text(fav.asLowerCase),
+      // child: ListTile(
+      //   leading: Icon(Icons.favorite),
+      //   title:Text(fav.asLowerCase)),
+    );
   }
 }
 
@@ -70,9 +195,8 @@ class BigCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.surface
-    );
+    final style = theme.textTheme.displayMedium!
+        .copyWith(color: theme.colorScheme.surface);
 
     return Card(
       color: theme.colorScheme.primary,
@@ -82,7 +206,8 @@ class BigCard extends StatelessWidget {
         child: Text(
           pair.asLowerCase,
           style: style,
-          semanticsLabel: "${pair.first} ${pair.second}",),
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
