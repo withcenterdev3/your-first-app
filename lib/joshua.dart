ddfddcd02b51dd5cad3a4f7fb45ca1620ba13dc2
QCU <QCU@DESKTOP-7GI2R8Q>
2023-08-25 14:21:48 +0800

    Create Project

--- lib/joshua.dart
+++ lib/joshua.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(JoshuaPage());
+}
 
 class JoshuaPage extends StatelessWidget {
   const JoshuaPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Name App',
+        theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange)),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Joshua'),
-          actions: <Widget>[
-            IconButton(
-                icon: const Icon(Icons.home),
-                tooltip: 'Home',
-                onPressed: () {
-                  Navigator.pop(context);
-                }),
-          ],
-        ),
-        body: const Text('Start here'));
+      body: Column(
+        children: [
+          const Text('A Random idea'),
+          Text(appState.current.asLowerCase)
+        ],
+      ),
+    );
   }
 }
