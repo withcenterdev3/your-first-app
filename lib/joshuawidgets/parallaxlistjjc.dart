7a0f17e62f9528a16fbc67d17fdd2af0fd40da67
Kin <withcenter.dev4@gmail.com>
2023-08-31 18:39:49 +0800

    parallax update; has an error

--- lib/joshuawidgets/parallaxlistjjc.dart
+++ lib/joshuawidgets/parallaxlistjjc.dart
@@ -110,10 +110,11 @@ class LocationListItem extends StatelessWidget {
 }
 
 class ParallaxFlowDelegate extends FlowDelegate {
-  ParallaxFlowDelegate(
-      {required this.scrollable,
-      required this.listItemContext,
-      required this.backgroundImageKey});
+  ParallaxFlowDelegate({
+    required this.scrollable,
+    required this.listItemContext,
+    required this.backgroundImageKey,
+  }) : super(repaint: scrollable.position);
 
   final ScrollableState scrollable;
   final BuildContext listItemContext;
@@ -126,13 +127,39 @@ class ParallaxFlowDelegate extends FlowDelegate {
     );
   }
 
+  @override
   void paintChildren(FlowPaintingContext context) {
-    // TODO: implement paintChildren
+    final scrollableBox = scrollable.context.findRenderObject() as RenderBox;
+    final listItemBox = listItemContext.findRenderObject() as RenderBox;
+    final listItemOffset = listItemBox.localToGlobal(
+        listItemBox.size.centerLeft(Offset.zero),
+        ancestor: scrollableBox);
+
+    final viewportDimension = scrollable.position.viewportDimension;
+    final scrollFraction =
+        (listItemOffset.dy / viewportDimension).clamp(0.0, 1.0);
+
+    final verticalAlignment = Alignment(0.0, scrollFraction * 2 - 1);
+
+    final backgroundSize =
+        (backgroundImageKey.currentContext!.findRenderObject() as RenderBox)
+            .size;
+    final listItemSize = context.size;
+    final childRect =
+        verticalAlignment.inscribe(backgroundSize, Offset.zero & listItemSize);
+
+    context.paintChild(
+      0,
+      transform:
+          Transform.translate(offset: Offset(0.0, childRect.top)).transform,
+    );
   }
 
   @override
   bool shouldRepaint(ParallaxFlowDelegate oldDelegate) {
-    return scrollable != oldDelegate.scrollable || ;
+    return scrollable != oldDelegate.scrollable ||
+        listItemContext != oldDelegate.listItemContext ||
+        backgroundImageKey != oldDelegate.backgroundImageKey;
   }
 }
 
